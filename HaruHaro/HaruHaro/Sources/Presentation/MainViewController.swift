4b7166b4a29d5139c29ecab1f6807457a372a60a
Crois0509 <crois0509@naver.com>
2025-10-10 21:54:25 +0900

    ⚒️[proj] 프로젝트 기초 세팅

---
+++ HaruHaro/HaruHaro/Sources/Presentation/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaruHaro
+//
+//  Created by 장상경 on 10/10/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
