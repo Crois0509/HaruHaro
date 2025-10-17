c368ca8060bb5cf235f87a444da0cc5032be6b36
Crois0509 <crois0509@naver.com>
2025-10-17 17:37:05 +0900

    ✨[feat] DiaryRepository에 메서드 추가
    검색 단어를 통해 일기를 불러올 수 있는 멧드 추가

--- HaruHaro/HaruHaro/Sources/Domain/Protocols/DiaryRepositoryProtocols.swift
+++ HaruHaro/HaruHaro/Sources/Domain/Protocols/DiaryRepositoryProtocols.swift
@@ -23,6 +23,7 @@ protocol DiaryRepository {
     func save(entry: DiaryEntry) -> Completable
     func fetchAllEntries() -> Observable<[DiaryEntry]>
     func fetchEntries(for date: Date) -> Observable<[DiaryEntry]>
+    func fetchEntries(for content: String) -> Observable<[DiaryEntry]>
     func delete(entry: DiaryEntry) -> Completable
     
     // MARK: - Sync Status
